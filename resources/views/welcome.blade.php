86a091ded52e79df7fc5ad92c4a57e77da917a45
Keir Forster <keirforster@gmail.com>
2018-04-08 13:57:26 -0700

    updated homepage link to about page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,7 @@
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="contact">contact us</a>
-                    <a href="https://github.com/laravel/laravel">about</a>
+                    <a href="about">about</a>
                 </div>
             </div>
         </div>
